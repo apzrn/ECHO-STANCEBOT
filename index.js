b0ac054c52d32df30fc9b713bd47861d62e612ec
apzrn <85966982+apzrn@users.noreply.github.com>
2021-07-05 23:13:11 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.on('guildMemberAdd', (member) => {
     const channel = member.guild.channels.cache.find(ch=>ch.name==="welcome")
     let embed = new Discord.MessageEmbed()
     .setTitle('Welcome to ECHO Stance!')
-    .setDescription(`Head to #ticket and Open a Ticket by Reacting Witb the Emoji at the Bottom, Support Will be With You in Less Than 5 Minutes Unless Away, You Will Recieve a Response in the Same Day!\n**Current Member Count:** ${member.guild.memberCount}\n**Owner:** ${member.guild.owner.user.tag}`)
+    .setDescription(`Head to #ticket and Open a Ticket by Reacting With the Emoji at the Bottom, Support Will be With You in Less Than 5 Minutes Unless Away, You Will Recieve a Response in the Same Day!\n**Current Member Count:** ${member.guild.memberCount}\n**Owner:** ${member.guild.owner.user.tag}`)
     .setColor('#65707A')
     .setFooter(member.guild.name, member.guild.iconURL())
     .setThumbnail(member.user.avatarURL());

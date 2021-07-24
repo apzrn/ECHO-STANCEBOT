8585fcfb9391c26d2c3462ce654cdfaa5f4f32bf
apzrn <85966982+apzrn@users.noreply.github.com>
2021-07-24 20:14:32 +0100

    Add files via upload

--- commands/outfit.js
+++ commands/outfit.js
@@ -1,7 +1,21 @@
 exports.run = async (bot,message,args) => {
     let member = message.mentions.members.first();
     if(!member) { message.channel.send('Crew Outfit - With Names - #crewoutfit');} else {
-        message.channel.send(`Hats - Any : Mask - Any : Tops - Black Utility Top : Pants ~ Black Low Crotch Pants or Black Joggers (Glitch) : Shoes ~ White Hi Top Sneakers or White Sport Shoes :Gloves ~ White Cotton Gloves ${member.user.tag}`)
+        message.channel.send(`
+        Crew Outfit:
+
+        Hats - Any
+        
+        Mask - Any
+        
+        Tops - Black Utility Top
+        
+        Pants - Black Low Crotch Pants or Black Joggers
+        
+        Shoes ~ White Hi Top Sneakers or White Sport Shoes 
+        
+        Gloves ~ White Cotton Gloves`)
+
     }
   }
   
